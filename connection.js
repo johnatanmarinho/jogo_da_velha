cabb879078837260aecaff2efa9fe06863fd8966
johnatanmarinho <johnatan.marinho@gmail.com>
2020-02-19 17:11:31 -0300

    .

---
+++ connection.js
@@ -0,0 +1,70 @@
+let servidor = new Peer("servidorvelha");
+let peer = null;
+let player = 0;
+let vezDeQuem = true; // se true jogador da bolinha
+let servConn = null;
+let game = null;
+
+function startGame(id){
+    game = new Tabuleiro(id);
+
+    game.init();
+
+    document.addEventListener('click', function(){
+        let board = document.getElementById('board');
+        setTimeout(function(){
+            servConn.send(game.logicalBoard);
+            game.checkWin();
+        },2);
+    });
+}
+servidor.on('open', function(id){
+    console.log("server connectado");
+    startGame("X");
+    servidor.on('connection', function(conn){
+        servConn = conn;
+        conn.on('open', function(data){
+           console.log(servidor.connections);
+            
+        });
+        conn.on('error', function(err){
+            console.log(err.type);
+            
+        });
+        conn.on('data', function(data){
+            update(data);
+        });
+    });
+});
+
+
+
+servidor.on('error', function(error){
+    if(error.type === "unavailable-id"){
+        console.log("inicia o player");
+        let peer = new Peer();
+        player = "X";    
+        startGame("O");
+        peer.on('open', function(id){
+            console.log("player " + id);
+            servConn = peer.connect('servidorvelha');
+
+            servConn.on('open', function(data){
+                console.log("connectado com sucesso");
+                
+                
+            })
+
+            servConn.on('data', function(data){
+                update(data);
+            });
+        });
+    }
+});
+
+
+function update( aff ){
+    game.logicalBoard = aff;
+    game.update();
+    game.checkWin();
+}
---
+++ game.js
@@ -0,0 +1,111 @@
+
+
+function Tabuleiro(id){
+    this.player = id;
+    this.board = document.createElement('div');
+    this.logicalBoard = [
+        '','','',
+        '','','',
+        '','',''
+    ];
+
+    this.init = function (){
+        
+        this.board.setAttribute('id', 'board');
+       
+        
+        for(let i = 0; i < 9; i++){
+            let self = this;
+            let cell = document.createElement('div');
+            cell.setAttribute('id', i);
+            cell.setAttribute('class', 'board-space');
+            cell.innerHTML="  ";
+            cell.onclick = function(){
+
+              if(this.innerHTML !== "X" && this.innerHTML !== "O"){
+                  console.log('tabuleiro ',self.logicalBoard);
+                  
+                  let index = this.getAttribute('id');
+                  self.logicalBoard[index] = self.player;
+                  self.update();
+              }
+              
+            }
+            this.board.appendChild(cell);
+            
+        }
+        this.board.appendBefore(document.getElementById("main"));
+    }
+
+    this.update = function(){
+        for(let i = 0 ; i < 9; i++ ){            
+            this.board.childNodes[i].innerHTML = this.logicalBoard[i];
+        }
+    }
+
+    this.checkWin = function(){
+        let spaces = this.board.childNodes;
+        let winner = null;
+        for(let i =0; i < 7; i = i + 3){
+            if( this.equals3(  
+                this.logicalBoard[i], 
+                this.logicalBoard[i+1],
+                this.logicalBoard[i+2])){
+                
+                winner = this.logicalBoard[i];
+            }
+        }
+
+        for(let i =0; i < 3; i++){
+            if( this.equals3(  
+                this.logicalBoard[i], 
+                this.logicalBoard[i+3],
+                this.logicalBoard[i+6])){
+                
+                winner = this.logicalBoard[i];
+            }
+        }
+
+        if(this.equals3(
+            this.logicalBoard[0],
+            this.logicalBoard[4],
+            this.logicalBoard[8]
+        ))
+        winner = this.logicalBoard[0];        
+        if(this.equals3(
+            this.logicalBoard[2],
+            this.logicalBoard[4],
+            this.logicalBoard[6]
+        ))
+        winner = this.logicalBoard[2];
+
+
+        if(winner !==null && winner === this.player){
+            alert("você ganhou!");
+            this.logicalBoard = [
+                '','','',
+                '','','',
+                '','',''
+            ];
+            this.update();
+        }else if(winner !== null){
+            alert("você perdeu");
+            this.logicalBoard = [
+                '','','',
+                '','','',
+                '','',''
+            ];
+            this.update();
+        }
+
+    }
+
+    this.equals3 = function(a, b, c){
+        return a == b && b == c && a != '';
+    }
+
+    Element.prototype.appendBefore = function( element ) {
+        element.parentNode.insertBefore(this, element);
+    }, false;
+
+}
